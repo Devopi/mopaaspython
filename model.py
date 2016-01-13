59a4fa8a640fe1af87a3e8bdb83fca64932ffce3
tempdban <temp@dban.tk>
2016-01-13 21:18:59 +0800

    test

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import web
 
-db = web.database(dbn='mysql', db='todo', user='justin')
+db = web.database(dbn='mysql', db='d3b422e343b4b42b88d85706d06320d59', user='e4048619-c3c3', host='192.168.1.11', pw='b442e4ec-e842', port=3306,)
 
 def get_todos():
     return db.select('todo', order='id')
